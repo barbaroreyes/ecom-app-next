d67454e4bb829c5738e7e1c53bc2b7fbf15532f7
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-05 22:26:37 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import 'tachyons';
-
+import { Button ,Icon} from 'semantic-ui-react'
 export default function Home() {
   return (
     <div className ='bg-grey'>
        <h1>Hello Next</h1>
+       <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+    <Icon flipped='horizontally' name='cloud' />
+    <Icon flipped='vertically' name='cloud' />
     </div>
   )
 }
